87e5b3e426eb0a8cfb019ce083db9ced15b34117
ardhikaxx <ardhikayanuar58@gmail.com>
2026-09-12 14:13:17 +0700

    remove onboarding - app langsung buka ke home

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'core/theme/app_theme.dart';
-import 'features/onboarding/onboarding_screen.dart';
 import 'features/home/home_screen.dart';
 
 void main() {
@@ -19,49 +17,7 @@ class CatatUntungApp extends ConsumerWidget {
       title: 'Catat Untung',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const _AppInitializer(),
+      home: const HomeScreen(),
     );
-  }
-}
-
-class _AppInitializer extends StatefulWidget {
-  const _AppInitializer();
-
-  @override
-  State<_AppInitializer> createState() => _AppInitializerState();
-}
-
-class _AppInitializerState extends State<_AppInitializer> {
-  bool? _isOnboardingCompleted;
-
-  @override
-  void initState() {
-    super.initState();
-    _checkOnboarding();
-  }
-
-  Future<void> _checkOnboarding() async {
-    final prefs = await SharedPreferences.getInstance();
-    final completed = prefs.getBool('onboarding_completed') ?? false;
-    if (mounted) {
-      setState(() => _isOnboardingCompleted = completed);
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (_isOnboardingCompleted == null) {
-      return const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
-        ),
-      );
-    }
-
-    if (!_isOnboardingCompleted!) {
-      return const OnboardingScreen();
-    }
-
-    return const HomeScreen();
   }
 }
--- lib/routing/app_router.dart
+++ lib/routing/app_router.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import '../features/onboarding/onboarding_screen.dart';
 import '../features/home/home_screen.dart';
 import '../features/products/products_screen.dart';
 import '../features/products/product_form_screen.dart';
@@ -16,10 +15,6 @@ final goRouter = GoRouter(
   navigatorKey: _rootNavigatorKey,
   initialLocation: '/',
   routes: [
-    GoRoute(
-      path: '/onboarding',
-      builder: (context, state) => const OnboardingScreen(),
-    ),
     GoRoute(
       path: '/',
       builder: (context, state) => const HomeScreen(),
